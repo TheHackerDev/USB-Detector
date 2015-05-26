2d096ed696ebd3e9308e790fb00f656dacf8c8d6
Aaron Hnatiw <aaron@aaronhnatiw.ca>
2015-05-26 08:30:52 -0400

    Successfully detects insertion and removal of USB devices.

---
+++ USB Detector/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace USB_Detector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_output
+            // 
+            this.txt_output.AcceptsReturn = true;
+            this.txt_output.Location = new System.Drawing.Point(102, 77);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ReadOnly = true;
+            this.txt_output.Size = new System.Drawing.Size(576, 363);
+            this.txt_output.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.txt_output);
+            this.Name = "Form1";
+            this.Text = "USB Detector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_output;
+    }
+}
+
---
+++ USB Detector/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USB_Detector
+{
+    public partial class Form1 : Form
+    {
+        // For detecting device insertion and removal.
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVALCOMPLETE = 0x8004;
+        private const int WM_DEVICECHANGE = 0x0219;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Overriding the base method in order to handle the WM_DEVICECHANGE event
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    // The WParam value identifies what is occurring.
+                    if (m.WParam.ToInt32() == DBT_DEVICEARRIVAL)
+                    {
+                        txt_output.Text = "Device inserted.";
+                    }
+                    else if (m.WParam.ToInt32() == DBT_DEVICEREMOVALCOMPLETE)
+                    {
+                        txt_output.Text = "Device removed.";
+                    }
+                    break;
+            }
+            
+            // Continue to do what was originally done with the message
+            base.WndProc(ref m);
+
+
+        }
+    }
+}
